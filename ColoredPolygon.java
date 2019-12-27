c6fe030d0effa22842a69c50ea795fcb2b684a36
3rdCore <tom.marty@polytechnique.edu>
2019-12-27 15:27:35 +0100

    implementation

--- ColoredPolygon.java
+++ ColoredPolygon.java
@@ -1,34 +1,33 @@
-Import Image2d;
+
 import java.io.*;
+import java.util.*;
 
 public class ColoredPolygon {
-  private int[] xcoords;
-  private int[] ycoords;
-  private String color;
+	private int[] xcoords;
+	private int[] ycoords;
+	private String color;
 
 //Several constructors are implemented.
 
-  public ColoredPolygon(int[] xcoords, int[] ycoords, String color){
-    this.xcoords=xcoords;
-    this.ycoords=ycoords;
-    this.color=color;
-  }
+	public ColoredPolygon(int[] xcoords, int[] ycoords, String color) {
+		this.xcoords = xcoords;
+		this.ycoords = ycoords;
+		this.color = color;
+	}
 
-  public ColoredPolygon(String input,String color){
-    String[] couples =input.split(",");
-    int[]xcoords=new int[couples.size()];
-    int[]ycoords=new int[couples.size()];
-    int i=0;
-    for (String couple: couples) {
-      couple=couple.replace('(', '').replace(')','').split(",");
-      xcoords[i]=couple[0];
-      ycoords[i]=couple[1];
-    }
-    this.xcoords=xcoords;
-    this.ycoords=ycoords;
-    this.color=color;
-  }
-
-
+	public ColoredPolygon(String input, String color) {
+		String[] couples = input.split(",");
+		int[] xcoords = new int[couples.length];
+		int[] ycoords = new int[couples.length];
+		int i = 0;
+		for (String couple : couples) {
+			String[] Couple = couple.replace("(", "").replace(")", "").split(",");
+			xcoords[i] = Integer.parseInt(Couple[0]);
+			ycoords[i] = Integer.parseInt(Couple[1]);
+		}
+		this.xcoords = xcoords;
+		this.ycoords = ycoords;
+		this.color = color;
+	}
 
 }
--- ColoredPolygons.java
+++ ColoredPolygons.java
@@ -1,38 +1,36 @@
 
 import java.io.*;
-
+import java.util.*;
 
 public class ColoredPolygons {
-  private java.util.List<ColoredPolygon> data;
-  private String color;
+	private java.util.List<ColoredPolygon> data;
+	private String color;
 
-  public ColoredPolygons(String filename,String color){        //constructeur à partir d'un .txt
-      List<String> F = readFile(filename);
-      for (String ligne:F){
-        this.data.add(new coloredPolygon(ligne,color));
+	public ColoredPolygons(String filename, String color) { // constructeur à partir d'un .txt
+		List<String> F = readFile(filename);
+		for (String ligne : F) {
+			this.data.add(new ColoredPolygon(ligne, color));
 
-      }
-      this.color=color;
+		}
+		this.color = color;
+	}
+
+	private List<String> readFile(String filename) // récupération sous la forme d'une list de String d'un fichier
+													// .txt
+	{
+		List<String> records = new ArrayList<String>();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				records.add(line);
+			}
+			reader.close();
+			return records;
+		} catch (Exception e) {
+			System.err.format("Exception occurred trying to read '%s'.", filename);
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
-
-  private List<String> readFile(String filename)  //récupération sous la forme d'une list de String d'un fichier .txt
-  {
-    List<String> records = new ArrayList<String>();
-    try
-    {
-      BufferedReader reader = new BufferedReader(new FileReader(filename));
-      String line;
-      while ((line = reader.readLine()) != null)
-      {
-        records.add(line);
-      }
-      reader.close();
-      return records;
-    }
-    catch (Exception e)
-    {
-      System.err.format("Exception occurred trying to read '%s'.", filename);
-      e.printStackTrace();
-      return null;
-    }
-  }
